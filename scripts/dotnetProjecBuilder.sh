922f7e1995e5ea2e9c67ba535d0a1e4df37a2ecc
Kterlecki <terlecki1995@gmail.com>
2023-07-27 07:49:14 +0100

    Add dotnet commands and bbug fixes complete

--- scripts/dotnetProjecBuilder.sh
+++ scripts/dotnetProjecBuilder.sh
@@ -3,38 +3,28 @@ set -eu
 
 dotnetProjectBuilder(){
     github_project_name=$1
-
+    project_type=$2
 
     if [ $? -ne 0 ]; then
         echo "Error: Required credentials for new dotnet project not entered"
         return 1
     fi
 
-    if [ [$github_project_name =~ [^\/]*(?=\.git$)]]; then
-        project_name="${BASH_REMATCH[0]}"
+    if [[ $github_project_name =~ /([^/]+)\.git$ ]]; then
+        project_name="${BASH_REMATCH[1]}"
     else
         echo "Invalid github URL format"
         return 1
+    fi
 
-    git clone $github_project_name
-    cd 
+    git clone "$github_project_name"
+    cd "$project_name"
+    dotnet new "$project_type" -o src/ --name "$project_name"
+    dotnet new xunit -o test/ --name "$project_name"Test
+    dotnet new sln --name "$project_name"
+    dotnet sln add **/*.csproj
+    dotnet add "test/"$project_name"Test.csproj" reference "src/$project_name.csproj"
+    dotnet sln list
 }
 
-
-
-## notes
-# [^\/]*: Character Class and Quantifier
-
-# [^\/]: The caret ^ inside square brackets [] denotes a character class. It means "match any character except the ones listed in the brackets." 
-# In this case, we are matching any character except a forward slash.
-# *: The asterisk is a quantifier and means "match zero or more occurrences" of the preceding pattern, which is the character class [^\/].
-# Purpose: This part matches any sequence of characters (repository name) that does not contain forward slashes. 
-# It will match characters from the beginning of the input string until it encounters a forward slash or reaches the end of the string.
-# (?=\.git$): Positive Lookahead Assertion
-
-# (?= ... ): This is a special syntax in regex for a positive lookahead assertion. It allows you to check that a pattern is followed by another pattern without including the following pattern in the actual match.
-# \.: The backslash \ is used to escape the dot . character, which is a special character in regex and matches any character except a newline.
-# git: This is the literal string "git" that we are looking for.
-# $: The dollar sign at the end anchors the pattern to match only at the end of the string.
-# Purpose: This part ensures that the pattern we are interested in (the repository name) is followed by ".git" at the end of the string. 
-# The lookahead assertion checks that the pattern exists but does not include it in the match. This means the ".git" extension won't be part of the extracted repository name.
+dotnetProjectBuilder "$1" "$2"
